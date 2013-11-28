00a36227c5aea2dc4609431a545849e4a2f3efe2
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-11-28 20:33:31 +0100

    Initial empty Rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IntercityTest::Application.config.secret_key_base = '1ea8b8288beec60eab40b56abf4d49bd8f6b517116326cee0afb26cdf0ba83a66bd811aee43f7305d12b7343089cc6d3937f74694fc132331b88637576071f4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IntercityTest::Application.config.session_store :cookie_store, key: '_intercity_test_session'

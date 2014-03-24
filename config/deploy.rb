3e7bed23723af2e3b7fd83e330203a350c48a808
Michiel Sikkes <michiel.sikkes@gmail.com>
2014-03-24 10:00:59 +0100

    Update to Ruby 2.1.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,15 @@ require 'bundler/capistrano'
 
 set :application, "intercity_sample_app"
 set :repository, "git@github.com:intercity/intercity_sample_app.git"
-set :ssh_options, { forward_agent: true }
+set :rails_env, "staging"
+set :ssh_options, { forward_agent: true, port: 2222 }
 set :default_run_options, { pty: true }
 set :user, "deploy"
 set :use_sudo, false
 set :default_environment, {
   "PATH" => "/opt/rbenv/shims:/opt/rbenv/bin:$PATH"
 }
-
-server "95.85.61.92", :web, :app, :db, :primary => true
+server "localhost", :web, :app, :db, :primary => true
 
 after "deploy:finalize_update", "link:database"
 
